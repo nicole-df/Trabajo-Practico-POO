06d61d7c19e5a1c5e72d638346a41809f10f887f
nicole-df <153960581+nicole-df@users.noreply.github.com>
2024-11-20 18:44:05 -0300

    String to Dic class

---
+++ str2dic.py
@@ -0,0 +1,17 @@
+class Str2Dic():
+    def __init__(self, schema, separator=","):
+        self.schema = schema.split(separator)
+        self.separator = separator
+
+    def convert(self, row):
+        tmp = row.split(self.separator)
+        if len(tmp) == len(self.schema):
+            i = 0
+            d = {}
+            while i<len(tmp):
+                key = self.schema[i]
+                val = tmp[i]
+                d[key] = val
+                i=i+1
+            return d
+        
